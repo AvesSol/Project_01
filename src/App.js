703d6fd29db615166743e0db6ae4f7b413864b30
0shivamh <shivamhande0000@gmail.com>
2022-02-14 15:13:29 +0530

    change: onload loader color and size

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React, {Suspense} from "react";
+import LoadingComponent from "./Components/loadingComponent.component";
 import {BrowserRouter as Router, Route } from "react-router-dom";
 import Home from './Home';
 import Login from "./Components/login.component";
 import Register from "./Components/register.component";
-import Navbar from "./Components/navbar.component";
 import About1 from "./about";
+const Navbar =  React.lazy(() => import("./Components/navbar.component"));
+// const Footer =  React.lazy(() => import("./Components/footer.component"));
 
 function App() {
   return (
     <div className="App">
       <Router>
+      <Suspense fallback={<LoadingComponent/>}>
         <Navbar/>
+
         <Route exact path='/' component={Home} />
         <Route path='/login' component={Login} />
         <Route path='/register' component={Register} />
         <Route path='/about' component={About1} />
+
+        {/* <Footer/> */}
+        </Suspense>
       </Router>
     </div>
   );
--- src/Components/loadingComponent.component.js
+++ src/Components/loadingComponent.component.js
@@ -1,9 +1,13 @@
+import logo from "../assets/logo/cplogo.png"
 const LoadingComponent = () => {
     return(
+        <>
         <div className="parrent-div">
-            <div className="zoom-in-zoom-out"><h1 className="display-4">{'{'}</h1></div>
-            <div className="zoom-out-zoom-in"><h1 className="display-4">{'}'}</h1></div>
-        </div>
+            <div className="zoom-in-zoom-out"><p className="display-1 loading-font">{'{'}</p></div>
+            <div className="zoom-out-zoom-in"><p className="display-1 loading-font">{'}'}</p></div>
+         </div>
+            {/* <img src={logo} className="img-fluid" /> */}
+</>
     )
 }
 export default LoadingComponent;
